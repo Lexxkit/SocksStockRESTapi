9766113ee814085068577e8bc16bd361c8af3a11
Lexxkit <lexxkit@gmail.com>
2023-03-07 15:25:44 +0000

    init

---
+++ src/main/java/com/lexxkit/socksstockaut/SocksStockAutApplication.java
@@ -0,0 +1,13 @@
+package com.lexxkit.socksstockaut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksStockAutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksStockAutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lexxkit/socksstockaut/SocksStockAutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexxkit.socksstockaut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksStockAutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
